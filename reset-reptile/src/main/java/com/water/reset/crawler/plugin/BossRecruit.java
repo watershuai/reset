90e6ad5a6953815c102d933e6e04295484e98e8a
shuaidi_liu <lw051010>
2019-08-04 16:42:00 +0800

    增加类

---
+++ reset-reptile/src/main/java/com/water/reset/crawler/plugin/BossRecruit.java
@@ -0,0 +1,15 @@
+package com.water.reset.crawler.plugin;
+
+import com.water.reset.crawler.CrawlerJob;
+
+/**
+ * @Description: TODO
+ * @author:water
+ * @Data:
+ */
+public class BossRecruit extends CrawlerJob {
+    @Override
+    public void grasp() {
+
+    }
+}
