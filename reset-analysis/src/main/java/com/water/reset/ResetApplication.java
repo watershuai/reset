3c9fc61c0a55e04c5bc81a817dc9d160d7fec557
shuaidi_liu <lw051010>
2019-08-29 15:57:07 +0800

    增加类

---
+++ reset-analysis/src/main/java/com/water/reset/ResetApplication.java
@@ -0,0 +1,13 @@
+package com.water.reset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResetApplication.class, args);
+    }
+
+}
