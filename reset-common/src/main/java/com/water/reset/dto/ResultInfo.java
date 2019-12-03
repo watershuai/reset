3c656623ce16e29d46cd462caf4b16f18541cf35
Administrator <1659336988@qq.com>
2019-12-03 16:00:57 +0800

    公共类

---
+++ reset-common/src/main/java/com/water/reset/dto/ResultInfo.java
@@ -0,0 +1,54 @@
+package com.water.reset.dto;
+
+public class ResultInfo {
+    private boolean success;
+    private String msg;
+    private String resultCode;
+    private Object data;
+    public ResultInfo(){
+    }
+    public ResultInfo(boolean success, String resultCode, String msg){
+        this.success=success;
+        this.resultCode=resultCode;
+        this.msg=msg;
+    }
+    public ResultInfo(boolean success, String resultCode, String msg,Object data){
+        this.success=success;
+        this.resultCode=resultCode;
+        this.msg=msg;
+        this.data=data;
+
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
---
+++ reset-common/src/main/java/com/water/reset/utils/Tool.java
@@ -0,0 +1,50 @@
+package com.water.reset.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * @Description: TODO
+ * @author:water
+ * @Data:
+ */
+public class Tool {
+
+    /**
+     * @author:water
+     * @Data 2019/07/22
+     * @Description 根据时间生成唯一id
+     */
+    public static String getToken(int num){
+        SimpleDateFormat format=new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        return format.format(new Date())+getRandom(num);
+    }
+
+    /**
+     * @param num 随机数个数
+     * @author:water
+     * @Data 2019/07/22
+     * @Description 获取随机数
+     */
+    public static String getRandom(int num){
+        if (num < 1){
+            return "";
+        }
+        StringBuilder builder=new StringBuilder();
+        Random random=new Random();
+        for (int i=0; i<num; i++){
+            builder.append(random.nextInt(10));
+        }
+        return builder.toString();
+    }
+    /**
+     * @author:water
+     * @Data 2019/09/06
+     * @Description 生成唯一token
+     */
+    public static String getToken(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+}
